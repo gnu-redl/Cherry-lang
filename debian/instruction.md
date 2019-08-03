85859f24744e3fea2a498e2321979dc9d827f0af
RedL <52675202+RedL-GNU-GPL@users.noreply.github.com>
2019-08-03 16:51:08 +0300

    redl

--- debian/instruction.md
+++
@@ -1,6 +0,0 @@
-How to install
-
-1.go in dir with cherry lang (file install)
-2.write 'chmod ugo+x install'
-3.write './install'
-4.reboot system
--- win32/bin/treat.cpp
+++
@@ -1,8 +0,0 @@
-		for(int x =0;x<maxsimbhol;x++) {
-				if(codest[x] == '\0') {
-					newer += " ";
-					break;
-				}
-				newer += codest[x];
-			
-		}
